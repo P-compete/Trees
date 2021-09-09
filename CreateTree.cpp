af60b9d7959f8cbf44f3ea3a029291d513d5b3de
P-compete <singhsomil3310@gmail.com>
2021-09-09 13:22:12 +0530

    Initial Commit

---
+++ CreateTree.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{ int data;
+	node *left,*right;
+
+ node(int item)
+ {
+	 data=item;
+	 left=NULL;
+	 right=NULL;
+ }
+};
+
+
+int main()
+{
+  node *root=new node(1);
+  root->left=new node(2);
+  root->right=new node(3);
+  root->left->left=new node(5);
+  root->right->left=new node(4);
+
+return 0;
+}
+
